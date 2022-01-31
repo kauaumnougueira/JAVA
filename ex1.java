ac3120d5603f57b6e1793581b44c3e795290bc84
Kauã Nogueira <81389414+kauaumnougueira@users.noreply.github.com>
2022-01-31 18:57:18 -0300

    Update and rename Main.java to ex1.java

--- ex1.java
+++ ex1.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
  
 public class Main{
-    public static void main(String[] args){
+    public static void ex1(String[] args){
 
         Scanner sc = new Scanner(System.in);
 
