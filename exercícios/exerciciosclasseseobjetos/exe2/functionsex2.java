e09183645fc48cf7fe5cab473164c705bf826868
kauaumnougueira <kaua_kna@hotmail.com>
2022-02-17 19:17:22 -0300

    ex2 classes e objetos

---
+++ exercícios/exerciciosclasseseobjetos/exe2/functionsex2.java
@@ -0,0 +1,19 @@
+package exercícios.exerciciosclasseseobjetos.exe2;
+
+public class functionsex2 {
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public void increaseSlary(double percentage){
+        grossSalary += grossSalary * percentage / 100;
+    }
+    public double netSalary(){
+        double editedSalary = grossSalary - tax;
+        return editedSalary;
+    }
+    public String toString(){
+        String editedLog = name + ", R$ " + String.format("%.2f", netSalary());
+        return editedLog;
+    }
+}
---
+++ exercícios/exerciciosclasseseobjetos/exe2/programex2.java
@@ -0,0 +1,33 @@
+package exercícios.exerciciosclasseseobjetos.exe2;
+import java.util.Scanner;
+
+public class programex2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        functionsex2 employee;
+        employee = new functionsex2();
+
+        System.out.println("-----------------------------------------");
+        System.out.println("Nome do funcionário: ");
+        employee.name = sc.next();
+        System.out.println("Salário bruto do funcionário (em reais): ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.println("Imposto do seu salário(em reais): ");
+        employee.tax = sc.nextDouble();
+        System.out.println("------------------------------------------");
+        System.out.println("Dados recebidos: ");
+        System.out.println(employee);
+
+        System.out.println("------------------------------------------------");
+        System.out.println("Insira o incremento de salário(Em porcentagem): ");
+        double percentage = sc.nextDouble();
+        employee.increaseSlary(percentage);
+        
+        System.out.println("---------------");
+        System.out.println("Salário atual: ");
+        System.out.println(employee);
+        
+        sc.close();
+    }
+}
